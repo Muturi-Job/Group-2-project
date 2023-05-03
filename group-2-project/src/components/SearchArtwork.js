7cae958eb10b7e9dc4657d6b48d2d0058db9069f
keruboelizabeth02 <keruboelizabeth02@gmail.com>
2023-05-03 15:44:33 +0300

    displayed the searched images

--- group-2-project/src/components/SearchArtwork.js
+++ group-2-project/src/components/SearchArtwork.js
@@ -1,8 +1,9 @@
 import React, {useState,useEffect} from "react";
 import Search from "./Search";
+import ArtCard from "./ArtCard";
 
 function SearchArtWorks(){
-    const [query, setQuery] = useState('');
+    const [searchTerm, setSearchTerm] = useState("");
     const [IDs, setIDs] = useState([])
     const [items, setItems] = useState([]);
 
@@ -13,7 +14,31 @@ function SearchArtWorks(){
         .catch(err=> console.log(err))
     
     },[])
+    useEffect(()=>{
+        IDs.map(id =>
+            fetch(`https://api.artic.edu/api/v1/artworks/${id}`)
+            .then(data =>data.json())
+            .then(item => setItems((prevItem => [...prevItem,item.data])))
+            .catch(err => console.log(err))
+            )
+    },[IDs])
 
+    return(
+        <div>
+            <input type="text" className="search" placeholder="Search artworks.."
+            value={searchTerm} onChange={(e)=> setSearchTerm(e.target.value)} 
+            />
+            <h1>hello</h1>
+            <div className="parent">
+                {
+                    items.filter(item =>item.title.toLowerCase().includes(searchTerm.toLowerCase()))
+                    .map((item, index) => 
+                    <ArtCard key= {index} item={item} />
+                    )
+                }
+            </div>
+        </div>
+    )
   
        
 }
