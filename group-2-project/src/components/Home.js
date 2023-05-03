7ed367b33c2b506644fae4acf15adc33a782acb5
Job Muturi <muirimuturi17@gmail.com>
2023-05-03 11:51:40 +0300

    Fixed Navbar

--- group-2-project/src/components/Home.js
+++ group-2-project/src/components/Home.js
@@ -1,43 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-import ArtworksList from './ArtworksList';
-import SearchArtworks from './SearchArtwork';
-import ArtCard from './ArtCard';
 
 function Home() {
   return (
-    <Router>
-      <div>
-        <nav className='menu'>
-          <ul>
-            <li>
-              <Link to="/">HOME</Link>
-            </li>
-            <li>
-              <Link to="/artworks">ARTWORKS</Link>
-            </li>
-            <li>
-              <Link to="/search">SEARCH</Link>
-            </li>
-            <li>
-              <Link to="/ArtCard">ART</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <header>
-          {/* <h1>Header Title</h1> */}
-        </header>
-
-        <Routes>
-          <Route exact path="/" />
-          <Route path="/artworks" component={ArtworksList} />
-          <Route path="/search" component={SearchArtworks} />
-          <Route path="/artcard" component={ArtCard} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="home" >
+   
+  </div>
   );
 }
-
 export default Home;
